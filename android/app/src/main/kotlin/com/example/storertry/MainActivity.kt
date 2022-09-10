5698c92c61b2b6e8da90179cd3a8e3041455cd92
Eng kareen magdy <75484642+kareenmagdy@users.noreply.github.com>
2022-09-10 07:54:09 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/storertry/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.storertry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:storertry/screens/home_page.dart';
+import 'package:storertry/screens/update_product_page.dart';
+
+void main() {
+  runApp(const StoreApp());
+}
+
+class StoreApp extends StatelessWidget {
+  const StoreApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        HomePage.id : (context) => HomePage(),
+        UpdateProductPage.id : (context) => UpdateProductPage(),
+
+      },
+      initialRoute: HomePage.id,
+    );
+  }
+}
